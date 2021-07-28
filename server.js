50fa70231285e697b5b350b3e46e9497e6ba623b
Bhoomika Madhukar <bhoomikamadhukar@Bhoomikas-MacBook-Pro.local>
2021-07-28 14:30:08 -0600

    Set up the mongodb cluster on atlas and test App.js in a different port

--- server.js
+++ server.js
@@ -9,7 +9,18 @@ app.get('/',(req,res)=>{
     res.json({message:"Hello Nu"})
 })
 
-const port = process.env.PORT || 3000;
+const URI = process.env.MONGODB_URL
+mongoose.connect(URI,{
+    useCreateIndex:true,
+    useFindAndModify:false,
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+},error=>{
+    if(error) throw error;
+    console.log('Connected to mongo')
+})
+
+const port = process.env.PORT || 8000;
 app.listen(port,()=>{
     console.log("Success. App is running on",port)
 })
