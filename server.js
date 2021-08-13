f94a91690dddc7fa3c43d468aa9cfd399103a089
Bhoomika Madhukar <bhoomikamadhukar@Bhoomikas-MacBook-Pro.local>
2021-08-12 20:45:06 -0400

    Deploy

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
+const path = require('path')
 
 const app = express()
 app.use(express.json())
@@ -23,6 +24,12 @@ mongoose.connect(URI,{
     if(error) throw error;
     console.log('Connected to mongo')
 })
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('ui/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'ui', 'build', 'index.html'))
+    })
+}
 
 const port = process.env.PORT || 8000;
 app.listen(port,()=>{
